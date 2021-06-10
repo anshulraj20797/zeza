fda0fc9fa88f48b94cc9b680814ba09f413f8341
anshulraj20797 <anshulraj20797@gmail.com>
2021-06-10 23:22:57 +0530

    setting updated

--- zeza/settings.py
+++ zeza/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'od9iwp&3p-ey+)&t+p(!u!6@v7n4vopd^wl&gfmi)wi#ac52#8'
+SECRET_KEY = '***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
